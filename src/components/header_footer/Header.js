9f9f63d2e6f373280cacfadf06e7da81354499ed
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-12-20 20:39:55 -0600

    Adding the menu icon from the react Material UI lybrary

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -2,7 +2,7 @@ import React,  { Component } from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
 class Header extends Component {
@@ -16,9 +16,16 @@ class Header extends Component {
            }}>
                <Toolbar>
                    <div className="header_logo">
-                   <div className="font_righteous header_logo_venue">Mr. Hustle,Loyalty & Respect</div>
                    <div className="header_logo_title">John Cena</div>
+                   <div className="font_righteous header_logo_venue">Mr. Hustle,Loyalty & Respect</div>
                    </div>
+               <IconButton
+               aria-label="Menu"
+               color="inherit"
+               onClick={() => console.log("Open Mother Fucker")}
+               >
+                   <MenuIcon/>
+               </IconButton>
                </Toolbar>
            </AppBar>
         )
