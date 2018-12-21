09006d73c7eb8a0b8f0c024c9209d67f2699a418
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-12-20 19:49:02 -0600

    Uloading theupdated package.json file with the react material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import Header from './components/header_footer/Header'
 
 class App extends Component {
   render() {
     return (
       <div>
-       <h1>Hello world</h1>
+       <Header />
       </div>
     );
   }
--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,14 +1,16 @@
-import React,  { component } from 'react';
+import React,  { Component } from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material_ui/core/Toolbar';
-import Menu from '@material_ui/core/Menu';
-import IconButton from '@material_ui/core/IconButton';
+import ToolBar from '@material-ui/core/Toolbar';
+import Menu from '@material-ui/core/Menu';
+import IconButton from '@material-ui/core/IconButton';
 
 class Header extends Component {
     render() {
         return (
-            <div>Header</div>
+           <AppBar>
+               Header
+           </AppBar>
         )
     }
 }
