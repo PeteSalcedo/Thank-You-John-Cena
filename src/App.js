8a71c88b29d8633ee6b4eea3911580484524d3b6
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-12-23 23:05:39 -0600

    Uploading thereact sllider component with some awsome images from JC himself

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import './resources/style.css'
 import Header from './components/header_footer/Header'
 
+import Featured from './components/Featured';
+
 class App extends Component {
   render() {
     return (
       <div className="App" style={{ height:"1500px",background:'cornflowerblue' }}>
        <Header />
+       <Featured />
       </div>
     );
   }
---
+++ src/components/Featured/Carrousel.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Slider from 'react-slick'
+
+import image_1 from '../../resources/images/image_1.jpeg'
+import image_2 from '../../resources/images/image_2.jpg'
+import image_3 from '../../resources/images/image_3.jpeg'
+
+const Carrousel = () => {
+
+    const settings = {
+        dots:false,
+        infinite:true,
+        autoplay:true,
+        speed:500
+    }
+    return (
+        <div className="carrousel_wrapper"
+        style={{
+                
+               height:'${window.innerHeight}px',
+               overflow:'hidden' 
+            }}
+            > 
+           <Slider {...settings}>
+                <div>
+                   <div 
+                        className="carrousel_image"
+                        style={{
+                            background:`url(${image_1})`,
+                            height:`${window.innerHeight}px`
+                        }}
+                   ></div>
+                </div>
+                <div>
+                    <div 
+                        className="carrousel_image"
+                        style={{
+                            background:`url(${image_2})`,
+                            height:`${window.innerHeight}px`
+                        }}
+                   ></div>
+                </div>
+                <div>
+                    <div 
+                        className="carrousel_image"
+                        style={{
+                            background:`url(${image_3})`,
+                            height:`${window.innerHeight}px`
+                        }}
+                   ></div>
+                </div>
+            </Slider>       
+            
+     </div>
+    );
+};
+
+export default Carrousel;
