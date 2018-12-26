779fabdeca73ea1f4a8c72512a99cbdfff2a0da8
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-12-25 19:29:35 -0600

    Fixed the height ration for the carousel

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Header from './components/header_footer/Header'
 
 import Featured from './components/Featured';
 import VunueNfo from './components/venueNfo';
+import Highlights from './components/Highlights'
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height:"1500px",background:'cornflowerblue' }}>
+      <div className="App" style={{ height:"1800px",background:'cornflowerblue' }}>
        <Header />
        <Featured />
        <VunueNfo />
+       <Highlights/>
       </div>
     );
   }
