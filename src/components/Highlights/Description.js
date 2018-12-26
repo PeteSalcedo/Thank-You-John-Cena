5065787a026a00195b4aeb2b202956c01a54b52b
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-12-25 19:00:45 -0600

    Uploading the about me section

--- src/components/Highlights/Description.js
+++ src/components/Highlights/Description.js
@@ -5,9 +5,17 @@ const Description = () => {
     return(
         <Fade>
           <div className="center_wrapper">
-            <h2>Highlights</h2>
+            <h2>About John Cena</h2>
            </div>
            <div className="highlight">
+             
+John Felix Anthony Cena Jr. (/ˈsiːnə/; born April 23, 1977) is an American professional wrestler, actor, rapper, and television host. He is signed to WWE, where he appears for both the Raw and SmackDown brands.
+
+Cena gained fame in the WWE in the early 2000s, portraying a trash-talking rapper.[8][9] This success promoted Cena to act as the company's franchise player[10] and its public face,[11] which later prompted a transition to his current character, which he describes as a "goody-two shoes Superman".[12] Under his current incarnation, Cena has attained mainstream fame, and has won 25 championships throughout his career, with 16 reigns as a world champion.[Note 1][13] His 16 world title wins are tied with Ric Flair's, per WWE records.[14] Cena is a five-time United States Champion and a four-time world tag team champion. He is also a Money in the Bank ladder match winner (2012), a two-time Royal Rumble winner (2008, 2013), and a three-time Superstar of the Year Slammy Award winner (2009, 2010, 2012). He has also headlined WWE's flagship event WrestleMania on five occasions (2006, 2007, 2011, 2012, and 2013) as well as many other WWE pay-per-views. Despite his success, Cena's character is met with mixed critical reception, and he is considered one of the most polarizing professional wrestlers ever.[15] Cena's net worth is estimated to be around $55 million dollars.
+
+Outside of wrestling, Cena released the album You Can't See Me (2005), which gained platinum certification in the United States. Cena has also enjoyed success as an actor, starring in The Marine (2006), 12 Rounds (2009), Legendary (2010), The Reunion (2011), Trainwreck (2015), Ferdinand (2017), Blockers (2018), and Bumblebee (2018).[16] He has also made several guest appearances on television, and was the host of the reality show American Grit.[17]
+
+Cena is also known for his involvement in numerous philanthropic causes, namely the Make-A-Wish Foundation, where he has granted more than 500 wishes, the most in Make-A-Wish history.
 
            </div>
         </Fade>
