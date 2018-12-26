26968e0e4441e827a5df7cb31fcfa6e17495456e
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-12-25 18:02:44 -0600

    adding the files for the highlights component

---
+++ src/components/Highlights/Description.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Description = () => {
+    return(
+        <Fade>
+          <div className="center_wrapper">
+            <h2>Highlights</h2>
+           </div>
+           <div className="highlight">
+
+           </div>
+        </Fade>
+    )
+}
+
+export default Description;
