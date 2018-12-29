d93592b04355348f80f8ac35db59324f74e60860
PeteSalcedo <Peter.salcedo.diaz@gmail.com>
2018-12-28 21:24:45 -0600

    Uploading the react componetn for the pricing component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import './resources/style.css'
-import Header from './components/header_footer/Header'
+import './resources/styles.css';
+import { Element } from 'react-scroll';
 
-import Featured from './components/Featured';
+
+import Header from './components/header_footer/Header';
+import Featured from './components/featured';
 import VunueNfo from './components/venueNfo';
-import Highlights from './components/Highlights'
+import Highlight from './components/Highlights';
+import Pricing from './components/pricing';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height:"1800px",background:'cornflowerblue' }}>
-       <Header />
-       <Featured />
-       <VunueNfo />
-       <Highlights/>
+      <div className="App" style={{ height:"1500px",background:'cornflowerblue'}}>
+        <Header/>
+
+        <Element name="featured">
+          <Featured/>
+        </Element>
+
+        <Element name="venuenfo">
+          <VunueNfo/>
+        </Element>
+        
+        <Element name="highlights">
+          <Highlight/>
+        </Element>
+        
+        <Element name="pricing">
+          <Pricing/>
+        </Element>
       </div>
     );
   }
